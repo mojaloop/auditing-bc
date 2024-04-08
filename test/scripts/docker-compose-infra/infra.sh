d3abb86b66834d8997f99bc4bfcffc7feda37b0b
Si Thu Myo <sithu.myo@thitsaworks.com>
2024-04-08 18:23:29 +0630

    fix: try deploy the whole infra

---
+++ test/scripts/docker-compose-infra/infra.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+rm -rf ./exec
+wait
+mkdir ./exec/
+cp ./.env.sample exec/.env
+# docker compose -f ./docker-compose-infra.yml --env-file ./exec/.env down -v --rmi all
+wait
+mkdir ./exec/certs ./exec/esdata01 ./exec/kibanadata ./exec/logs \
+    ./exec/tigerbeetle_data ./exec/mongodb_data \
+    ./exec/prometheus_etc ./exec/prometheus_data \
+    ./exec/grafana_data
+docker compose -f ./docker-compose-infra.yml --env-file ./exec/.env up -d
+wait
+
